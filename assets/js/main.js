8ab3115f2ba3ff94bb582a81ab718f67b7367400
ralphocdol <ralph.ocdol63@gmail.com>
2020-09-18 21:06:05 +0800

    remove http(s) protocol on CDNs

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,107 +2,110 @@
 import { name, sidebar, experiences } from './data.js';
 
 (async () => {
-    // Populate sidebar
-    document.getElementById('sidebar-items').innerHTML = sidebar
-        .map((name, index) => {
-            let active = '';
-            if (index === 0) active = ` class="active"`;
-            return `<a href="#${name}"${active}>${name}</a>`;
-        })
-        .join('');
+     // Populate sidebar
+     document.getElementById('sidebar-items').innerHTML = sidebar
+          .map((name, index) => {
+               let active = '';
+               if (index === 0) active = ` class="active"`;
+               return `<a href="#${name}"${active}>${name}</a>`;
+          })
+          .join('');
 
-    // Sidebar content import
-    const getContent = async (content) => {
-        return await fetch(`assets/pages/${content}.html`)
-            .then((response) => response.text())
-            .catch((error) => {
-                throw new Error(error);
-            });
-    };
+     // Sidebar content import
+     const getContent = async (content) => {
+          return await fetch(`assets/pages/${content}.html`)
+               .then((response) => response.text())
+               .catch((error) => {
+                    throw new Error(error);
+               });
+     };
 
-    // Initial Page load
-    const webtitle_name = `${name.fullname().toLowerCase().replace(/\s/g, '')}`;
-    const urlHash = window.location.hash;
-    const initialPageName = urlHash ? urlHash.substr(1) : sidebar[0];
-    await loadContent(initialPageName);
+     // Initial Page load
+     const webtitle_name = `${name.fullname().toLowerCase().replace(/\s/g, '')}`;
+     const urlHash = window.location.hash;
+     const initialPageName = urlHash ? urlHash.substr(1) : sidebar[0];
+     await loadContent(initialPageName);
 
-    // selected item on sidebar
-    document.getElementById('sidebar-items').addEventListener('click', async (event) => {
-        await loadContent(event.target.innerText);
-        [...event.target.parentElement.children].forEach((eachItem) => {
-            eachItem.removeAttribute('class', 'active');
-        });
-        event.target.setAttribute('class', 'active');
-    });
+     // selected item on sidebar
+     document.getElementById('sidebar-items').addEventListener('click', async (event) => {
+          // multiple clicks
+          if (/\r|\n/.exec(event.target.innerText) !== null) return;
 
-    async function loadContent(pageName) {
-        document.getElementById('content').innerHTML = '';
-        document.getElementById('webtitle').innerHTML = `${webtitle_name} | ${pageName}`;
-        document.getElementById('content').innerHTML = await getContent(pageName);
-        try {
-            eval(pageName + 'FN()');
-        } catch (e) {
-            console.error(pageName + 'FN() does not exist!');
-        }
-    }
+          await loadContent(event.target.innerText);
+          [...event.target.parentElement.children].forEach((eachItem) => {
+               eachItem.removeAttribute('class', 'active');
+          });
+          event.target.setAttribute('class', 'active');
+     });
 
-    async function experienceFN() {
-        const eachposition = (positions) => {
-            return positions
-                .map((position) => {
-                    const workdetails = position.workdetails.map((eachworkdetails) => `<li>${eachworkdetails}</li>`).join('');
+     async function loadContent(pageName) {
+          document.getElementById('content').innerHTML = '';
+          document.getElementById('webtitle').innerHTML = `${webtitle_name} | ${pageName}`;
+          document.getElementById('content').innerHTML = await getContent(pageName);
+          try {
+               eval(pageName + 'FN()');
+          } catch (e) {
+               console.error(pageName + 'FN() does not exist!');
+          }
+     }
+
+     async function experienceFN() {
+          const eachposition = (positions) => {
+               return positions
+                    .map((position) => {
+                         const workdetails = position.workdetails.map((eachworkdetails) => `<li>${eachworkdetails}</li>`).join('');
+                         return `
+                              <div class="position_container">
+                                   <div class="position_name">${position.name} <span>(${position.workdate.from} – ${position.workdate.to})</span></div>
+                                   <div class="position_details">
+                                        <ul>
+                                             ${workdetails !== '' ? workdetails : '<li>None</li>'}
+                                        </ul>
+                                   </div>
+                              </div>
+                         `;
+                    })
+                    .join('');
+          };
+          const eachcompany = (data) => {
+               return `
+                    <div class="eachcompany">
+                         <div class="company_date">
+                              <div class="position_date">
+                                   <div>${data.workdate().to}</div>
+                                   <div><i class="fas fa-angle-up fa-sm"></i></div>
+                                   <div>${data.workdate().from}</div>
+                              </div>
+                              <div class="company_name">${data.company}</div>
+                         </div>
+                         ${eachposition(data.positions)}
+                    </div>
+               `;
+          };
+
+          document.getElementById('position_list').innerHTML = experiences
+               .map((experience) => {
                     return `
-          <div class="position_container">
-          <div class="position_name">${position.name} <span>(${position.workdate.from} – ${position.workdate.to})</span></div>
-            <div class="position_details">
-              <ul>
-                ${workdetails !== '' ? workdetails : '<li>None</li>'}
-              </ul>
-            </div>
-          </div>
-        `;
-                })
-                .join('');
-        };
-        const eachcompany = (data) => {
-            return `
-        <div class="eachcompany">
-          <div class="company_date">
-            <div class="position_date">
-              <div>${data.workdate().to}</div>
-              <div><i class="fas fa-angle-up fa-sm"></i></div>
-              <div>${data.workdate().from}</div>
-            </div>
-            <div class="company_name">${data.company}</div>
-          </div>
-          ${eachposition(data.positions)}
-        </div>
-      `;
-        };
-
-        document.getElementById('position_list').innerHTML = experiences
-            .map((experience) => {
-                return `
         <div class="eachexperience">
           ${eachcompany(experience)}
         </div>
       `;
-            })
-            .join('');
-    }
+               })
+               .join('');
+     }
 
-    // menu button animation
-    const menuBtn = document.querySelector('.menu-btn');
-    if (menuBtn) {
-        let menuOpen = false;
-        menuBtn.addEventListener('click', () => {
-            if (!menuOpen) {
-                menuBtn.classList.add('open');
-                menuOpen = true;
-            } else {
-                menuBtn.classList.remove('open');
-                menuOpen = false;
-            }
-        });
-    }
+     // menu button animation
+     const menuBtn = document.querySelector('.menu-btn');
+     if (menuBtn) {
+          let menuOpen = false;
+          menuBtn.addEventListener('click', () => {
+               if (!menuOpen) {
+                    menuBtn.classList.add('open');
+                    menuOpen = true;
+               } else {
+                    menuBtn.classList.remove('open');
+                    menuOpen = false;
+               }
+          });
+     }
 })();
